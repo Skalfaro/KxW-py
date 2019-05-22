533f5adc7a3f3b8bc83a24ddc4858605f8a8b47d
skalfaro <triv@hotmail.it>
2019-05-22 23:51:13 +0200

    calcolatore fatto

--- py101.py
+++ py101.py
@@ -1,1 +1,14 @@
 num1 = float(input("enter num1: "))
+op = input("enter operator: ")
+num2 = float(input("enter num2: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("no operator")
