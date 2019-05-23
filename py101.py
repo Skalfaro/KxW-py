bc4f434b9ed28971d833c3ab1ed8c321603f1424
skalfaro <triv@hotmail.it>
2019-05-23 20:43:18 +0200

    potenze hard

--- py101.py
+++ py101.py
@@ -1,3 +1,8 @@
-for shit in "culo acadamy":
-    print(shit)
+def raise_to_power(base, power):
+    result = 1
+    for index in range(power):
+        result = result * base
+    return result
 
+
+print(raise_to_power(3, 2))
