fda4dd51137f63f8da7e23129495a034adcb0a64
skalfaro <triv@hotmail.it>
2019-05-23 19:13:10 +0200

    loop

--- py101.py
+++ py101.py
@@ -1,14 +1,7 @@
-num1 = float(input("enter num1: "))
-op = input("enter operator: ")
-num2 = float(input("enter num2: "))
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "*":
-    print(num1 * num2)
-elif op == "/":
-    print(num1 / num2)
-else:
-    print("no operator")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+print("done loop")
+
